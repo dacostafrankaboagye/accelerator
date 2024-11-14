079ff81f3a59c1f13eb6adaedb8cabeaf8d69726
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-11-14 09:03:33 +0000

    add user repo

--- wk-3/UserApp/src/main/java/frank/userapp/repository/UserRepository.java
+++ wk-3/UserApp/src/main/java/frank/userapp/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package frank.userapp.repository;
 
+import frank.userapp.model.User;
+import java.util.List;
 
-public class UserRepository {
+public interface UserRepository {
+
+    User createUser(User user);
+    User getUserById(Long id);
+    User updateUser(Long id, User user);
+    void deleteUser(Long id);
+    List<User> getAllUsers();
 }
