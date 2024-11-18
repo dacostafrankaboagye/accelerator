9b9dc067299e18ea327adb62abaa91f8cc8b79ff
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-11-18 13:01:22 +0000

    Week 5 project setup

---
+++ wk-5/SecureShop/src/test/java/frank/secureshop/SecureShopApplicationTests.java
@@ -0,0 +1,13 @@
+package frank.secureshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
